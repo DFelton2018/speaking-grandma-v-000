3dd363e65a4c9d0166e30a51206bc9225e4e2329
Duane Felton <dfelton2010@gmail.com>
2018-09-12 14:20:24 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -21,9 +21,9 @@
 #end
 
 def speak_to_grandma(phrase)
-  if phrase != phrase.capitalize
+  if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.capitalize
+  elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   
   end
